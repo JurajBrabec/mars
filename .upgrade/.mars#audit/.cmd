83a7088db41f3e6314ad6166d5b00470e59a678f
Juraj Brabec <juraj@brabec.sk>
2018-04-18 15:36:57 +0200

    added qrs audit

--- .upgrade/.mars#audit/.cmd
+++ .upgrade/.mars#audit/.cmd
@@ -11,39 +11,26 @@ for /f "delims=" %%i in ("!folder!\..\") do set "folder=%%~fi"
 if not exist "!folder!\cmd" goto :setup
 set "root=%folder%"
 if "%root:~-1%"=="\" set root=%root:~0,-1%
-for /f %%i in (files\build) do set build=%%i
+set build=vpc-qrs-audit
 set "logfile=%root%\logs\update-%build%.log"
 if exist "%logfile%" del /q "%logfile%"
 :begin
-call :echo MARS update #%build% starting...
-for /f %%i in (%root%\build) do call :check %%i
+call :echo MARS %build% starting...
+for /f %%i in (%root%\build) do call :update %%i
 goto :end
 
-:check
-set _new=0
-for /f "delims=. tokens=1-3" %%a in ("%1") do (
-	for /f "delims=. tokens=1-3" %%d in ("%build%") do (
-		if %%d gtr %%a set _new=1
-		if %%e grt %%b set _new=1
-		if %%f geq %%c set _new=1
-	)
-)
-if "%_new%" equ "1" goto :ok
-call :echo Cannot continue. MARS already on build #%1
-set errorlevel=1
-goto :eof
-:ok
+:update
 call :echo Build #%1 found
 set result=0
 rem if "%result%" equ "0" call :service stop http
 rem if "%result%" equ "0" call :service stop db
 rem if "%result%" equ "0" call :delete
-if "%result%" equ "0" call :copy
+rem if "%result%" equ "0" call :copy
 rem if "%result%" equ "0" call :service start db
 rem if "%result%" equ "0" call :service start http
 if "%result%" equ "0" call :sql
 if "%result%" equ "0" goto :finish
-call :echo Error %result%. MARS update #%build% NOT successful.
+call :echo Error %result%. MARS %build% NOT successful.
 set errorlevel=%result%
 goto :eof
 :finish
@@ -52,14 +39,14 @@ goto :eof
 
 :delete
 call :echo Deleting file(s)...
-del /q "%root%\tmp\*.tmp" >>"%logfile%" 2>&1
-rmdir /s /q "%root%\www\phpmyadmin" >>"%logfile%" 2>&1
+rem del /q "%root%\tmp\*.tmp" >>"%logfile%" 2>&1
+rem rmdir /s /q "%root%\www\phpmyadmin" >>"%logfile%" 2>&1
 set result=%errorlevel%
 goto :eof
 
 :copy
 call :echo Copying file(s)...
-xcopy /e /i /k /y files "%root%" >>"%logfile%" 2>&1
+rem xcopy /e /i /k /y files "%root%" >>"%logfile%" 2>&1
 set result=%errorlevel%
 goto :eof
 
@@ -67,6 +54,18 @@ goto :eof
 if not exist .sql goto :eof
 call :echo Executing SQL...
 "%root%\bin\db\bin\mysql.exe" -u root -D mysql <.sql >>"%logfile%" 2>&1
+set result=%errorlevel%
+if "%result%" neq "0" goto :eof
+call :echo Importing QRS...
+"%root%\bin\db\bin\mysql.exe" -u root -D audit <qrs.sql >>"%logfile%" 2>&1
+set result=%errorlevel%
+if "%result%" neq "0" goto :eof
+call :echo Importing comments...
+"%root%\bin\db\bin\mysql.exe" -u root -D audit <comments.sql >>"%logfile%" 2>&1
+set result=%errorlevel%
+if "%result%" neq "0" goto :eof
+call :echo Executing audit...
+"%root%\bin\db\bin\mysql.exe" -u root -D audit --execute "call procedure_execute();" >>"%logfile%" 2>&1
 set result=%errorlevel%
 goto :eof
 
---
+++ .upgrade/.mars#audit/readme.txt
@@ -0,0 +1,16 @@
+MARS 4.1 VPC QRS Audit
+
+CHANGELOG:
+1.04.2018 Fresh VPC QRS Audit
+•	
+
+HOW-TO INSTALL:
+(Automatic execution)
+•	Remotely upload the ZIP file via Administration interface, or place the ZIP file to MARS www\upload folder
+•	Wait for next Scheduler execution (each 15 minutes)
+•	Check schedule.log/update-*.log for results
+(Manual execution)
+•	Extract the ZIP file to MARS folder with overwrite
+•	Open command prompt in MARS\#*
+•	Run .CMD
+•	Delete the #* folder once finished.
