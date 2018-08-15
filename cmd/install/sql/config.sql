d4a55a92618f793a3a5a5e8f9228c6c8df7f47b3
Juraj Brabec <juraj@brabec.sk>
2018-08-15 11:49:38 +0200

    simplified SQLs

--- cmd/install/sql/config.sql
+++ cmd/install/sql/config.sql
@@ -88,37 +88,41 @@ SELECT cs.*
 	ORDER BY cs.date,cs.time,cs.name
 ;
 
-INSERT INTO `config_timeperiods` (`ord`, `name`, `value`, `created`, `updated`, `obsoleted`) VALUES
-	(3, 'Last 12 hours', 'H-12::H', '2013-01-01 00:00:00', '2013-01-01 00:00:00', NULL),
-	(23, 'Last 12 months', 'M-12::M', '2013-01-01 00:00:00', '2017-09-08 10:52:13', NULL),
-	(24, 'Last 12 months (15.)', 'N-12::N', '2013-01-01 00:00:00', '2017-09-08 10:52:15', '2017-04-27 13:04:27'),
-	(3, 'Last 18 hours', 'H-18::H', '2018-04-27 15:05:50', '2018-04-27 15:06:16', NULL),
-	(13, 'Last 2 weeks', 'W-2::W', '2013-01-01 00:00:00', '2017-09-08 10:51:49', NULL),
-	(4, 'Last 24 hours', 'H-24::H', '2013-01-01 00:00:00', '2013-01-01 00:00:00', NULL),
-	(8, 'Last 3 days', 'D-3::D', '2013-01-01 00:00:00', '2013-01-01 00:00:00', NULL),
-	(19, 'Last 3 months', 'M-3::M', '2013-01-01 00:00:00', '2017-09-08 10:52:06', NULL),
-	(20, 'Last 3 months (15.)', 'N-3::N', '2013-01-01 00:00:00', '2017-09-08 10:52:08', '2017-04-27 13:04:30'),
-	(10, 'Last 30 days', 'D-30::D', '2013-01-01 00:00:00', '2013-01-01 00:00:00', NULL),
-	(14, 'Last 4 weeks', 'W-4::W', '2013-01-01 00:00:00', '2017-09-08 10:51:50', NULL),
-	(5, 'Last 48 hours', 'H-48::H', '2013-01-01 00:00:00', '2013-01-01 00:00:00', NULL),
-	(21, 'Last 6 months', 'M-6::M', '2013-01-01 00:00:00', '2017-09-08 10:52:10', NULL),
-	(22, 'Last 6 months (15.)', 'N-6::N', '2013-01-01 00:00:00', '2017-09-08 10:52:11', '2017-04-27 13:04:32'),
-	(9, 'Last 7 days', 'D-7::D', '2013-01-01 00:00:00', '2013-01-01 00:00:00', NULL),
-	(2, 'Last hour', 'H-1::H', '2013-01-01 00:00:00', '2013-01-01 00:00:00', NULL),
-	(17, 'Last month', 'M-1::M', '2013-01-01 00:00:00', '2017-09-08 10:52:02', NULL),
-	(18, 'Last month (15.)', 'N-1::N', '2013-01-01 00:00:00', '2017-09-08 10:52:04', '2017-04-27 13:04:34'),
-	(12, 'Last week', 'W-1::W', '2013-01-01 00:00:00', '2017-09-08 10:51:47', NULL),
-	(26, 'Last year', 'Y-1::Y', '2013-01-01 00:00:00', '2017-09-08 10:52:19', NULL),
-	(1, 'This hour', 'H::H+1', '2013-01-01 00:00:00', '2013-01-01 00:00:00', NULL),
-	(15, 'This month', 'M::M+1', '2013-01-01 00:00:00', '2017-09-08 10:51:53', NULL),
-	(16, 'This month (15.)', 'N::N+1', '2013-01-01 00:00:00', '2017-09-08 10:52:01', '2017-04-27 13:04:35'),
-	(11, 'This week', 'W::W+1', '2013-01-01 00:00:00', '2017-09-08 10:51:45', NULL),
-	(25, 'This year', 'Y::Y+1', '2013-01-01 00:00:00', '2017-09-08 10:52:17', NULL),
-	(6, 'Today', 'D::D+1', '2013-01-01 00:00:00', '2013-01-01 00:00:00', NULL),
-	(7, 'Yesterday', 'D-1::D', '2013-01-01 00:00:00', '2013-01-01 00:00:00', NULL);
+INSERT INTO `config_timeperiods` (`ord`, `name`, `value`) VALUES
+	(3, 'Last 12 hours', 'H-12::H'),
+	(23, 'Last 12 months', 'M-12::M'),
+#	(24, 'Last 12 months (15.)', 'N-12::N'),
+	(3, 'Last 18 hours', 'H-18::H'),
+	(13, 'Last 2 weeks', 'W-2::W'),
+	(4, 'Last 24 hours', 'H-24::H'),
+	(8, 'Last 3 days', 'D-3::D'),
+	(19, 'Last 3 months', 'M-3::M'),
+#	(20, 'Last 3 months (15.)', 'N-3::N'),
+	(10, 'Last 30 days', 'D-30::D'),
+	(14, 'Last 4 weeks', 'W-4::W'),
+	(5, 'Last 48 hours', 'H-48::H'),
+	(21, 'Last 6 months', 'M-6::M'),
+#	(22, 'Last 6 months (15.)', 'N-6::N'),
+	(9, 'Last 7 days', 'D-7::D'),
+	(2, 'Last hour', 'H-1::H'),
+	(17, 'Last month', 'M-1::M'),
+#	(18, 'Last month (15.)', 'N-1::N'),
+	(12, 'Last week', 'W-1::W'),
+	(26, 'Last year', 'Y-1::Y'),
+	(1, 'This hour', 'H::H+1'),
+	(15, 'This month', 'M::M+1'),
+#	(16, 'This month (15.)', 'N::N+1'),
+	(11, 'This week', 'W::W+1'),
+	(25, 'This year', 'Y::Y+1'),
+	(6, 'Today', 'D::D+1'),
+	(7, 'Yesterday', 'D-1::D');
 
-#INSERT INTO `config_towers` (`name`, `policyname`, `created`, `updated`, `obsoleted`) VALUES
-#	('Default', '.+', '2017-05-03 10:39:27', '2017-05-03 10:39:37', NULL);
+INSERT INTO `config_settings` (`name`,`value`) VALUES
+	( 'region', 'Default' ),
+	( 'bw_start', 'midnight' );
 
-#INSERT INTO `config_customers` (`name`, `policyname`, `created`, `updated`, `obsoleted`) VALUES
-#	('Default', '.+', '2016-12-14 15:06:46', '2017-04-19 15:31:24', NULL);
+#INSERT INTO `config_towers` (`name`, `policyname`) VALUES
+#	('Default', '.+');
+
+#INSERT INTO `config_customers` (`name`, `policyname`) VALUES
+#	('Default', '.+');
